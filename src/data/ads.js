ae6b151c48cef8076909b8332c10132ff522cef4
Lamin Mohamed <viralforyou2026@gmail.com>
2026-08-27 23:24:06 +0100

    Two ads: sidebar 300x250 and footer 728x90

--- src/data/ads.js
+++ src/data/ads.js
@@ -1,19 +1,5 @@
 export const adConfig = {
   enabled: true,
-  topBanner: {
-    enabled: true,
-    code: `<script>
-  atOptions = {
-    'key' : '31101887a6cf24579b7b1ce0fca9f598',
-    'format' : 'iframe',
-    'height' : 90,
-    'width' : 728,
-    'params' : {}
-  };
-</script>
-<script src="https://www.highrevenueformat.com/31101887a6cf24579b7b1ce0fca9f598/invoke.js"></script>`,
-    fallbackText: 'Ihre Werbung hier',
-  },
   sidebarBanner: {
     enabled: true,
     code: `<script>
@@ -26,6 +12,20 @@ export const adConfig = {
   };
 </script>
 <script src="https://www.highrevenueformat.com/f18ce742602d0a6a09dbeedbb488140d/invoke.js"></script>`,
+    fallbackText: 'Ihre Werbung hier',
+  },
+  footerBanner: {
+    enabled: true,
+    code: `<script>
+  atOptions = {
+    'key' : '31101887a6cf24579b7b1ce0fca9f598',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highrevenueformat.com/31101887a6cf24579b7b1ce0fca9f598/invoke.js"></script>`,
     fallbackText: 'Ihre Werbung hier',
   },
 };
